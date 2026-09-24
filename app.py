9ecac703d2ac00d0efc2af07a948208fbafcd93f
cwcarrillov <cwcarrillov@outlook.es>
2026-09-23 21:01:43 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,3 +2,6 @@ import streamlit as st
 st.title("Bolsa de Valores Quito BI")
 st.sidebar.title("Parámetros")
 st.write("Elaborado por: Carlos Carrillo")
+
+archivo = st.file_uploader("Cargue su archivo")
+
